c81271a63aa7f17efa264fad7adf4c08547c4fd5
eproskuryakov <proskur.egor@yandex.ru>
2023-10-24 17:12:14 +0300

    init project

---
+++ src/test/java/ru/vsu/cs/proskuryakov/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.proskuryakov.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
